263f410aecf8df6ae588b3d9770649134d049a06
NiteshHalai <nitesh.halai@gmail.com>
2023-02-07 21:31:24 +0300

    Update base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a BaseModel class
+"""
+
+
+class BaseModel:
+
+    """
+    This is a BaseModel class
+    """
+    
+    self.id = id
+    self.created_at = created_at
+    self.updated_at = updated_at
+    
+    def __str__(self):
+        return 'dummy'
